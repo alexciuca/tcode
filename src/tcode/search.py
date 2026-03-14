1d86691b4d3e8379a7692ece613c942cae29c91c
Andrei Cretu2 <andrei.cretu4676@gmail.com>
2026-03-14 19:32:17 -0400

    /comments

--- src/tcode/search.py
+++ src/tcode/search.py
@@ -3,6 +3,7 @@ from textual.screen import Screen
 from textual.widgets import Label, Button
 
 class SearchProblems(Screen):
+    # TODO: Add list of all problems with pagination (Late : optimize so it doesnt load all 2000+ problems at once, maybe like 20 per page)
     def compose(self) -> ComposeResult:
         yield Label("Search Problems")
         yield Button("Back", id="back-button")
